d2b46a411fa25681f1224acebe77bbdf7856ed84
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-05 14:48:21 -0300

    example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'widget-feedback';
+import { StyleSheet, View } from 'react-native';
+import { FeedbackWidget } from '@rodrigorbrg/widget-feedback';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <FeedbackWidget />
     </View>
   );
 }
---
+++ src/FeedbackWidget/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../theme';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.brand,
+    bottom: 0,
+    flex: 1,
+    position: 'absolute',
+  },
+});
