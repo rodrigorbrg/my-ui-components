df8a1858d1abd80dd721a09ca429669bc2cc2ff6
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-06 18:08:15 -0300

    form

--- src/FeedbackForm/feedback_form_widget.tsx
+++ src/FeedbackForm/feedback_form_widget.tsx
@@ -1,9 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  TouchableOpacity,
+  TextInput,
+  Text,
+  View,
+  Image,
+  ImageSourcePropType,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
+import { ArrowLeft } from 'phosphor-react-native';
 
-// import styles from './styles';
-// import { theme } from '../theme';
+import type { FeedbackType } from '../FeedbackWidget';
+import Button from '../Button';
+import SnapButton from '../SnapButton';
+import styles from './styles';
+import { theme } from '../theme';
 
-function FeedbackForm() {
-  return <></>;
+interface Props {
+  image: ImageSourcePropType;
+  title: string;
+  typeFeedBack: FeedbackType;
+  resetFeedback: () => void;
+}
+
+function FeedbackForm({ image, title, typeFeedBack, resetFeedback }: Props) {
+  const [feedback, setFeedback] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onChange = (
+    e: NativeSyntheticEvent<TextInputChangeEventData>
+  ): void => {
+    const value = e.nativeEvent.text;
+    setFeedback(value);
+  };
+
+  const submit = async () => {
+    const body = {
+      comment: feedback,
+      type: typeFeedBack,
+      // screenshot: screenShot,
+    };
+    setLoading(true);
+    await fetch('http://localhost:3001/feedback', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      // mode: 'no-cors',
+    });
+    setLoading(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.arrow} onPress={resetFeedback}>
+          <ArrowLeft
+            color={theme.colors.text_secondary}
+            size={24}
+            weight={'bold'}
+          />
+        </TouchableOpacity>
+        <View style={styles.title}>
+          <Image source={image} style={styles.image} />
+          <Text style={styles.text}>{title}</Text>
+        </View>
+        <View />
+      </View>
+      <TextInput
+        numberOfLines={5}
+        value={feedback}
+        onChange={onChange}
+        style={styles.form}
+      />
+      <View style={styles.footer}>
+        <SnapButton
+          screenshot={'123'}
+          setScreenshot={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+        <Button onPress={submit} loading={loading} disabled={false}>
+          <Text>Enviar feedback</Text>
+        </Button>
+      </View>
+    </View>
+  );
 }
 export default FeedbackForm;
--- src/FeedbackForm/styles.ts
+++ src/FeedbackForm/styles.ts
@@ -5,4 +5,42 @@ export default StyleSheet.create({
   container: {
     backgroundColor: theme.colors.surface_primary,
   },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 16,
+  },
+  arrow: {
+    marginStart: 16,
+  },
+  title: {
+    flexDirection: 'row',
+  },
+  text: {
+    color: theme.colors.text_primary,
+    fontSize: 18,
+    textAlign: 'center',
+    marginHorizontal: 8,
+  },
+  image: {
+    height: 24,
+    width: 24,
+  },
+  form: {
+    backgroundColor: theme.colors.surface_primary,
+    borderWidth: 1,
+    borderColor: theme.colors.stroke,
+    color: theme.colors.text_primary,
+    fontSize: 14,
+    marginHorizontal: 24,
+    // textAlign: 'left',
+  },
+  footer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 16,
+    marginTop: 8,
+    paddingHorizontal: 24,
+  },
 });
