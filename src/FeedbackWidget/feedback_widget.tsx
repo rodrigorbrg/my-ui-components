244d3fde36d7fe23d88500404ab030e045d088a9
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-06 09:34:04 -0300

    working android

--- src/FeedbackWidget/feedback_widget.tsx
+++ src/FeedbackWidget/feedback_widget.tsx
@@ -3,11 +3,12 @@ import { View } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 
 import styles from './styles';
+import { theme } from '../theme';
 
 function FeedbackWidget() {
   return (
     <View style={styles.container}>
-      <ChatTeardropDots />
+      <ChatTeardropDots color={theme.colors.text_on_brand_color} />
     </View>
   );
 }
