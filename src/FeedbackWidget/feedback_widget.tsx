cc20a1db06aff9d335fcdbff44ebcf859aab1902
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-06 07:53:59 -0300

    icone button

--- src/FeedbackWidget/feedback_widget.tsx
+++ src/FeedbackWidget/feedback_widget.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
 
 import styles from './styles';
 
 function FeedbackWidget() {
   return (
     <View style={styles.container}>
-      <Text>{'ICON'}</Text>
+      <ChatTeardropDots />
     </View>
   );
 }
