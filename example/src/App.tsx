a90043ff8c07e6efb996b04f6d3d0b55594dae87
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-10 02:27:50 -0300

    theme injected corretly

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import {
   ThemeProvider,
+  Theme,
   FeedbackWidget,
   FeedbackBody,
   Button,
@@ -27,8 +28,24 @@ export default function App() {
     }
   };
 
+  const theme: Theme = {
+    colors: {
+      brand: '#825700',
+      background: '#09090A',
+
+      surface_primary: '#18181B',
+      surface_secondary: '#27272A',
+
+      text_primary: '#F4F4F5',
+      text_secondary: '#A1A1AA',
+      text_on_brand_color: '#FFFFFF',
+
+      stroke: '#52525B',
+    },
+  };
+
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <View style={styles.container}>
         <FeedbackWidget submitCallback={submit}>
           <Button onPress={() => {}} style={styles.button}>
