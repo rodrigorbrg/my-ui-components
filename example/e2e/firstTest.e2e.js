ef3175329725f05dacf183e6b8b8336ff03a7dcf
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-27 10:12:56 -0300

    basic test

--- example/e2e/firstTest.e2e.js
+++ example/e2e/firstTest.e2e.js
@@ -1,4 +1,3 @@
-// import {device, expect, element, by} from 'detox';
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -9,6 +8,10 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.id('Example Components'))).toBeVisible();
+    await expect(element(by.id('Example Screen'))).toExist();
+  });
+
+  it('should click button on screen', async () => {
+    await element(by.text('Button 1')).tap();
   });
 });
