a0637c01db072a84eac94e22b44e3b56fad72de8
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-26 23:12:20 -0300

    detox init

---
+++ example/e2e/firstTest.e2e.js
@@ -0,0 +1,14 @@
+// import {device, expect, element, by} from 'detox';
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('Example Components'))).toBeVisible();
+  });
+});
